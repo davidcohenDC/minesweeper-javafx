50e57b5735910f29918121dc4a7965e404cae5aa
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-02-18 18:16:11 +0100

    Started a player interface with getters for name and score

---
+++ src/main/java/score/Player.java
@@ -0,0 +1,15 @@
+package score;
+
+public interface Player {
+    /**
+     * @return
+     * returns the name of the player 
+     */
+    String getName();
+
+    /**
+     * @return
+     * returns the score of the player
+     */
+    int getScore();
+}
