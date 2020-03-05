7bcbfc326390e6ceb793ecbecd9c20162e54515d
Lorenzo <lorenzo.morelli9@studio.unibo.it>
2020-03-05 15:51:56 +0100

    defined main methods

--- src/main/java/gameLogics/GameEngine.java
+++ src/main/java/gameLogics/GameEngine.java
@@ -1,4 +1,10 @@
 package gameLogics;
 
+import java.util.HashMap;
+
 public interface GameEngine {
+
+    void hit(Pair<Integer, Integer> coord);
+
+    HashMap<Pair<Integer, Integer>, Integer> getBoard();
 }
