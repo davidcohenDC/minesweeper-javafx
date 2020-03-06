4830827eff9cceeaa42d5d6b8f772ea417d4a007
Lorenzo <lorenzo.morelli9@studio.unibo.it>
2020-03-06 20:01:16 +0100

    Implemented the constructor

--- src/main/java/gameLogics/GameEngineImpl.java
+++ src/main/java/gameLogics/GameEngineImpl.java
@@ -4,22 +4,31 @@ import java.util.HashMap;
 
 public class GameEngineImpl implements GameEngine{
 
-    @Override
+    private final Board board;
+
+    public GameEngineImpl(int width, int height) {
+        final BoardBuilder bb = new BoardBuilderImpl().setWidth(width).setHeight(height);
+        for(int i=0; i<width; i++) {
+            for(int j=0; j<height; j++) {
+                final Box box = new BoxImpl(new Pair<>(i, j));
+                bb.addBox(box);
+            }
+        }
+        this.board = bb.build();
+    }
+
     public void hit(Pair<Integer, Integer> coord) {
 
     }
 
-    @Override
     public void setFlag(Pair<Integer, Integer> coord) {
 
     }
 
-    @Override
     public GameStatus status() {
         return null;
     }
 
-    @Override
     public HashMap<Pair<Integer, Integer>, Integer> getBoard() {
         return null;
     }
