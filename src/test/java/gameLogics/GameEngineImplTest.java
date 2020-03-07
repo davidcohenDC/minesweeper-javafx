f3316a6c9ea4489d2b814b497cf108a65b64de83
Lorenzo <lorenzo.morelli9@studio.unibo.it>
2020-03-07 15:22:12 +0100

    Created class for GameEngineImpl testing

---
+++ src/test/java/gameLogics/GameEngineImplTest.java
@@ -0,0 +1,30 @@
+package gameLogics;
+
+import org.junit.jupiter.api.Test;
+
+class GameEngineImplTest {
+
+    private static GameEngine ge;
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void initialize() {
+        ge = new GameEngineImpl(4, 4, 1);
+    }
+
+    @Test
+    void hit() {
+
+    }
+
+    @Test
+    void setFlag() {
+    }
+
+    @Test
+    void status() {
+    }
+
+    @Test
+    void getBoard() {
+    }
+}
