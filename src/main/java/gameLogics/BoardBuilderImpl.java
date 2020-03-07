e5fbbde44a49ba7ba2acb6c71e06325bf2740696
Lorenzo <lorenzo.morelli9@studio.unibo.it>
2020-03-07 16:17:49 +0100

    removed unused import

--- src/main/java/gameLogics/BoardBuilderImpl.java
+++ src/main/java/gameLogics/BoardBuilderImpl.java
@@ -1,8 +1,6 @@
 package gameLogics;
 
 import java.util.HashSet;
-import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class BoardBuilderImpl implements BoardBuilder {
