d6e6c4e7ed2f6c8fcc7e7324f8edca72a3c30373
Lorenzo <lorenzo.morelli9@studio.unibo.it>
2020-03-13 16:30:40 +0100

    tested bomb system

---
+++ src/test/java/gameLogics/BombGeneratorImplTest.java
@@ -0,0 +1,16 @@
+package gameLogics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BombGeneratorImplTest {
+
+    @Test
+    void next() {
+        final BombGenerator bg = new BombGeneratorImpl(20, 5);
+        for(int i=0; i<20; i++) {
+            System.out.println(bg.next());
+        }
+    }
+}
