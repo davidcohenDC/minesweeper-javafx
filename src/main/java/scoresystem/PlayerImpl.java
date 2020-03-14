3ebc3277dc2b3cf23e74ed5188e6c1576b038a03
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-14 14:16:20 +0100

    Started PlayerImpl

---
+++ src/main/java/scoresystem/PlayerImpl.java
@@ -0,0 +1,40 @@
+package scoresystem;
+
+import controlutility.Difficulty;
+import controlutility.Modality;
+
+public class PlayerImpl implements Player {
+
+    private final String name;
+    private final Modality gameMode;
+    private final Difficulty difficuly;
+    private final int score;
+    private boolean hasWon;
+
+    public PlayerImpl(final String name, final Modality gameMode, final Difficulty difficulty, final int score) {
+        this.name = name;
+        this.score = score;
+        this.gameMode = gameMode;
+        this.difficuly = difficulty;
+    }
+
+    @Override
+    public final void won() {
+        this.hasWon = true;
+    }
+
+    @Override
+    public final void lost() {
+        this.hasWon = false;
+    }
+
+    @Override
+    public final int getScore() {
+        return this.score;
+    }
+
+    @Override
+    public final Modality getModality() {
+        return this.gameMode;
+    }
+}
