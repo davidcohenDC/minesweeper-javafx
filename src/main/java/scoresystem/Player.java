9bdf97057301201e4fd8f039d6f97eb7576c94fa
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-14 14:05:29 +0100

    Added methods won and lost to PlayerInterface

--- src/main/java/scoresystem/Player.java
+++ src/main/java/scoresystem/Player.java
@@ -3,5 +3,20 @@ package scoresystem;
  * A Player.
  */
 public interface Player {
-    
+
+    /**
+     * The Player has won the game.
+     */
+    void won();
+
+    /**
+     * The Player has lost the game.
+     */
+    void lost();
+
+    /**
+     * @return
+     * Returns the player's score
+     */
+    int getScore();
 }
