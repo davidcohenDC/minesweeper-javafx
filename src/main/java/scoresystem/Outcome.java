e35a24f1f6a88b2ec9478834a78d2c15b366344d
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-16 17:56:12 +0100

    Created enum Outcome to describe weather a player won or lost

---
+++ src/main/java/scoresystem/Outcome.java
@@ -0,0 +1,17 @@
+package scoresystem;
+
+/**
+ * The outcome of a game.
+ */
+public enum Outcome {
+
+    /**
+     * The player won.
+     */
+    WON,
+
+    /**
+     * The player lost.
+     */
+    LOST;
+}
