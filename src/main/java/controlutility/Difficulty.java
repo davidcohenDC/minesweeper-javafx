22b24102267b3ea565c7b41ed7008ec8fe9380db
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-25 17:19:04 +0100

    Added names and getter for each Difficulty level

--- src/main/java/controlutility/Difficulty.java
+++ src/main/java/controlutility/Difficulty.java
@@ -7,22 +7,32 @@ public enum Difficulty {
     /**
      * Easy level: 9 x 9 with 10 mines.
      */
-    EASY, 
+    EASY("Easy"), 
 
     /**
      * Medium level: 16 x 16 with 40 mines.
      */
-    MEDIUM,
+    MEDIUM("Medium"),
 
     /**
      * Hard level: 30 x 16 with 90 mines.
      */
-    HARD, 
+    HARD("Hard"), 
 
     /**
      * Personalized level: the field can have the preferred dimension between 9 x 9 and 30 x 24 with a number of mines between 10 and 667. 
      * 
      * Score will not be calculated with this setting.
      */
-    PERSONALIZED;
+    PERSONALIZED("Personalized");
+
+    private final String name;
+
+    Difficulty(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
