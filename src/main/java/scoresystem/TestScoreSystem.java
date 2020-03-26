f37ec6fc2747edc3f16b871d7a74fe981816a10a
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-26 16:00:50 +0100

    Tested player functionalities

--- src/main/java/scoresystem/TestScoreSystem.java
+++ src/main/java/scoresystem/TestScoreSystem.java
@@ -20,45 +20,109 @@ class TestScoreSystem {
     private ScoreWriter sw;
     private File file;
 
+//TEST for PLAYER FUNCTIONALITIES
     @Test
     void playerGettersTest() {
         Player p = new PlayerImpl("luigi", Modality.STANDARD, Difficulty.MEDIUM);
+
         assertEquals("luigi", p.getName());
         assertEquals(Difficulty.MEDIUM, p.getDifficuly()); 
         assertEquals(Modality.STANDARD, p.getModality());
-
 
         //player has not won or lost yet so the result must not be available
         try {
             assertEquals(Optional.empty(), Optional.of(p.getResult()));
             fail("TRYING TO ACCESS WHEN FORBIDDEN");
         } catch (IllegalStateException e) {
+            System.out.println(e);
             assertEquals(IllegalStateException.class, e.getClass());
         }
 
+        //player has not won or lost yet so the score is not been achived yet
         try {
             assertEquals(Optional.empty(), Optional.of(p.getScore()));
             fail("NO SCORE TO BE ACCESSED");
         } catch (IllegalStateException e) {
+            System.out.println(e);
             assertEquals(IllegalStateException.class, e.getClass());
         }
-
-        p.lost();
-        assertEquals(GameStatus.LOSE, p.getResult());
-
-        //if player lost there is still no score to be taken care of
-        try {
-            assertEquals(Optional.empty(), Optional.of(p.getScore()));
-            fail("NO SCORE TO BE ACCESSED");
-        } catch (IllegalStateException e) {
-            assertEquals(IllegalStateException.class, e.getClass());
-        }
+        System.out.println();
     }
 
     @Test
+    void lostTest() {
+        Player p = new PlayerImpl("luigi", Modality.STANDARD, Difficulty.MEDIUM);
+
+        //player loses
+        p.lost();
+        assertEquals(GameStatus.LOSE, p.getResult());
+
+        //player lost so no score needs to be accessed
+        try {
+            assertEquals(Optional.empty(), Optional.of(p.getScore()));
+            fail("NO SCORE TO BE ACCESSED");
+        } catch (IllegalStateException e) {
+            System.out.println(e);
+            assertEquals(IllegalStateException.class, e.getClass());
+        }
+
+        //player shouldn't be able to change its status after winning or losing
+        try {
+            p.won(3);
+            fail("CANNOT WIN AFTER LOSING");
+        } catch (IllegalStateException e) {
+            System.out.println(e);
+            assertNotEquals(GameStatus.WIN, p.getResult());
+            assertEquals(IllegalStateException.class, e.getClass());
+        }
+
+        try {
+            assertEquals(Optional.empty(), Optional.of(p.getScore()));
+            fail();
+        } catch (IllegalStateException e) {
+            System.out.println(e);
+        }
+        System.out.println();
+    }
+
+    @Test
+    void wonTest() {
+        Player p = new PlayerImpl("luigi", Modality.STANDARD, Difficulty.EASY);
+
+        //player wins the game in 8 seconds
+        p.won(8);
+        assertEquals(GameStatus.WIN, p.getResult());
+        assertEquals(8, p.getScore());
+
+        //player shouldn't be able to change its status after winning or losing
+        try {
+            p.lost();
+            fail("CANNOT LOSE AFTER WINNING");
+        } catch (IllegalStateException e) {
+            System.out.println(e);
+            assertEquals(8, p.getScore());
+            assertEquals(IllegalStateException.class, e.getClass());
+        }
+
+        //player shouldn't be able to change its status after winning or losing
+        try {
+            p.won(3);
+            fail("CANNOT WIN TWICE");
+        } catch (IllegalStateException e) {
+            System.out.println(e);
+            assertNotEquals(3, p.getScore());
+            assertEquals(IllegalStateException.class, e.getClass());
+        }
+
+        System.out.println();
+
+    }
+
+//TEST for SCORE WRITING FUNCTIONALITIES
+    @Test
     void scoreFileDoesNotExistTest() {
 
-         file = new File(ROOT + Modality.STANDARD + FILE_SEPARATOR + Difficulty.HARD + FILE_EXTENCION);
+        file = new File(ROOT + Modality.STANDARD + FILE_SEPARATOR + Difficulty.HARD + FILE_EXTENCION);
 
          if (file.exists()) {
             file.delete();
