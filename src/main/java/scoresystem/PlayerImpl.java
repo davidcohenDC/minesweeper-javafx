60c2e55498024772c3fd557e57f624d82504914e
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-26 17:01:40 +0100

    Added a control in won()
    to keep score empty if difficulty is personalized

--- src/main/java/scoresystem/PlayerImpl.java
+++ src/main/java/scoresystem/PlayerImpl.java
@@ -24,7 +24,9 @@ public class PlayerImpl implements Player {
     public final void won(final int score) {
         check(!this.result.isEmpty(), "Player's result cannot be modified after its initial registration");
         this.result = Optional.of(GameStatus.WIN);
-        this.score = Optional.of(score);
+        if (!this.difficuly.equals(Difficulty.PERSONALIZED)) {
+            this.score = Optional.of(score);
+        }
     }
 
     @Override
