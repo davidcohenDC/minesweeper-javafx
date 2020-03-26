0aeb1aa6f7d3f54d934559a2653e5ec06cd2d184
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-27 00:33:13 +0100

    Added testing method to see if scores get registered in the right order
    also specified static imports instead of using 'import static
    org.junit.jupiter.api.Assertions.*'

--- src/main/java/scoresystem/TestScoreWriting.java
+++ src/main/java/scoresystem/TestScoreWriting.java
@@ -1,16 +1,28 @@
 package scoresystem;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
 
 import org.junit.jupiter.api.Test;
 
 import controlutility.Difficulty;
 import controlutility.Modality;
 
+/**
+ * Class to test score writing functionalities. 
+ *
+ * BEWARE! RUNNING THIS CLASS WILL ALTER ACTUAL SCOREFILES
+ */
 class TestScoreWriting {
 
     private static final String FILE_EXTENCION = ".txt";
@@ -20,12 +32,13 @@ class TestScoreWriting {
     private ScoreWriter sw;
     private Path path;
     private Player p;
+
     @Test
     void scoreFileDoesNotExistTest() {
         System.out.println("scoreFileDoesNotExistTest");
 
         path =  Path.of(ROOT + Modality.STANDARD.getDirectoryName() + FILE_SEPARATOR + Difficulty.EASY.getName() + FILE_EXTENCION); 
-        Player p = new PlayerImpl("luigi", Modality.STANDARD, Difficulty.EASY);
+        p = new PlayerImpl("luigi", Modality.STANDARD, Difficulty.EASY);
 
         try {
             Files.deleteIfExists(path);
@@ -41,22 +54,22 @@ class TestScoreWriting {
         assertTrue(Files.exists(path));
 
         //file should not be created if player is playing personalized modality
-        Player personalized = new PlayerImpl("personalized", Modality.STANDARD, Difficulty.PERSONALIZED);
+        p = new PlayerImpl("personalized", Modality.STANDARD, Difficulty.PERSONALIZED);
         path =  Path.of(ROOT + Modality.STANDARD.getDirectoryName() + FILE_SEPARATOR + Difficulty.PERSONALIZED.getName() + FILE_EXTENCION); 
 
         assertTrue(Files.notExists(path));
-        sw = new ScoreWriterImpl(personalized);
+        sw = new ScoreWriterImpl(p);
         assertFalse(Files.exists(path));
         System.out.println();
 
     }
 
     @Test
-    void notWritableScores() {
+    void notWritableScoresTest() {
         System.out.println("notWritableScores");
 
         //Personalized players' scores are not to be kept track of so nothing should happen
-        Player p = new PlayerImpl("luigi", Modality.STANDARD, Difficulty.PERSONALIZED);
+        p = new PlayerImpl("luigi", Modality.STANDARD, Difficulty.PERSONALIZED);
         p.won(8);
         sw = new ScoreWriterImpl(p);
         path = Path.of(ROOT + Modality.STANDARD.getDirectoryName() + FILE_SEPARATOR + Difficulty.PERSONALIZED.getName() + FILE_EXTENCION);
@@ -84,16 +97,25 @@ class TestScoreWriting {
     }
 
     @Test
-    void writeScoreForSinglePlayerModality() {
-        Player p = new PlayerImpl("luigi", Modality.BTT, Difficulty.MEDIUM);
+    void writeScoreForSinglePlayerModalityTest() {
+        p = new PlayerImpl("luigi", Modality.BTT, Difficulty.MEDIUM);
         path = Path.of(ROOT + p.getModality().getDirectoryName() + FILE_SEPARATOR + p.getDifficuly().getName() + FILE_EXTENCION);
-        //file must be empty in the begging
+        //file MUST be empty in the begging of this test
         try {
             Files.deleteIfExists(path);
         } catch (IOException e1) {
             fail("FILE WAS NOT CANCELLED AT THE BEGGING OF THIS TEST SO IT MAKES THE REST USELESS");
         }
         sw = new ScoreWriterImpl(p);
+
+        //trying to write before player finished the game 
+        sw.writeScore();
+        try {
+            assertTrue(Files.size(path) == 0L);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
         p.won(23);
         sw.writeScore();
 
@@ -105,6 +127,52 @@ class TestScoreWriting {
             //it means the file was not written it the correct way
             fail();
         }
+    }
+
+    @Test
+    void scoreFileIsInOrder() {
+        path = Path.of(ROOT + Modality.STANDARD.getDirectoryName() + FILE_SEPARATOR + Difficulty.MEDIUM.getName() + FILE_EXTENCION);
+
+        //file MUST be empty in the begging of this test
+        try {
+            Files.deleteIfExists(path);
+        } catch (IOException e1) {
+            fail("FILE WAS NOT CANCELLED AT THE BEGGING OF THIS TEST SO IT MAKES THE REST USELESS");
+        }
+
+        Random rnd = new Random();
+        List<Integer> expectedScoreBoard = new ArrayList<Integer>();
+        List<Integer> actualScoreBoard = new ArrayList<Integer>();
+        int score;
+        int numberOfPlayers = 100;
+
+        //genera un numero di player facendoli vincere con score casuali e scrivendo i loro punteggi 
+        for (int i = 0; i < numberOfPlayers; i++) {
+            p = new PlayerImpl("p" + i, Modality.STANDARD, Difficulty.MEDIUM);
+            sw = new ScoreWriterImpl(p);
+            score = rnd.nextInt(999);
+            expectedScoreBoard.add(score);
+            p.won(score);
+            sw.writeScore();
+        }
+
+        expectedScoreBoard.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(final Integer score1, final Integer score2) {
+                return score1 - score2;
+            }
+        });
+
+        try {
+            for (Object line : Files.lines(path).toArray()) {
+                String string = String.valueOf(line);
+                actualScoreBoard.add(Integer.valueOf(string.split("-")[1]));
+            }
+        } catch (IOException e) {
+                System.err.println("The lines from the file were not transfered correctly.");
+                System.err.println(actualScoreBoard);
+        }
+        assertEquals(expectedScoreBoard, actualScoreBoard);
     }
 
 }
