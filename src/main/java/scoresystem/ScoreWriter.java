83fb70e316f15686f42a85af9e3fceb68b682eb6
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-27 14:12:35 +0100

    Added javadoc to ScoreWriter interface

--- src/main/java/scoresystem/ScoreWriter.java
+++ src/main/java/scoresystem/ScoreWriter.java
@@ -7,5 +7,14 @@ import controlutility.Modality;
 
 public interface ScoreWriter extends Writer {
  
+    /**
+     * Returns the score board of a specified game mode in a specified difficulty.
+     * @param gameMode
+     * the game mode of the desired score board
+     * @param difficulty
+     * the difficulty of the desired score board
+     * @return
+     * returns a Map<String, Integer> where players' names are the keys associates with their best score
+     */
     Map<String, Integer> getScoreBoard(Modality gameMode, Difficulty difficulty);
 }
