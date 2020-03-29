2489d6c57fe5b9a0ceed1725eea4f47417aaa222
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-29 11:23:17 +0200

    Created Player Factory interface

---
+++ src/main/java/scoresystem/PlayerFactory.java
@@ -0,0 +1,42 @@
+package scoresystem;
+
+import controlutility.Difficulty;
+
+public interface PlayerFactory {
+
+    /**
+     * Creates a Player for Standard Mode.
+     * @param name
+     * The Players' name 
+     * @param difficulty
+     * The selected Difficulty
+     * @return
+     * A Player with no adversary
+     */
+    Player createPlayerForStandardMode(String name, Difficulty difficulty);
+
+    /**
+     * Creates a Player for Beat The Timer Mode.
+     * @param name
+     * The Players' name 
+     * @param difficulty
+     * The selected Difficulty
+     * @return
+     * A Player with no adversary
+     */
+    Player createPlayerForBeatTheTimerMode(String name, Difficulty difficulty);
+
+    /**
+     * Creates a Player for 1 versus 1 Mode.
+     * @param name
+     * The Players' name 
+     * @param difficulty
+     * The selected Difficulty
+     * @param adversaryName
+     * The current adversary
+     * @return
+     * A Player with adversary
+     */
+    Player createPlayerFor1vs1Mode(String name, Difficulty difficulty, String adversaryName);
+
+}
