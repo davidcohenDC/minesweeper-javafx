f2c4571307964702ca14be45934bc6892acf4f77
Maria Mengozzi <maria.mengozzi3@studio.unibo.it>
2020-03-29 22:36:13 +0200

    update statistic code

--- src/main/java/controllers/StatisticsController.java
+++ src/main/java/controllers/StatisticsController.java
@@ -1,6 +1,5 @@
 package controllers;
 
-import java.io.File;
 import java.io.IOException;
 
 import controlutility.Difficulty;
@@ -20,7 +19,6 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -32,22 +30,27 @@ import javafx.stage.Stage;
 public class StatisticsController implements StatisticsControllerInterface {
     private final Modality modality;
     private final String buttonText;
+    private final double oldHeight;
+    private final double oldWidth;
     @FXML
     private Text title;
     @FXML 
     private AnchorPane rootPane;
     @FXML
     private VBox vBox;
-    @FXML
-    private ScrollPane scrollPane;
+
 
     /**constructor that initialize field.
      * @param modality modality chosen.
      * @param buttonText set title in order the modality chosen.
+     * @param oldW weight of mainStatistic.
+     * @param oldH height of mainStatistic.
     */
-    public StatisticsController(final Modality modality, final String buttonText) {
+    public StatisticsController(final Modality modality, final String buttonText, final double oldW, final double oldH) {
         this.modality = modality;
         this.buttonText = buttonText;
+        this.oldWidth = oldW;
+        this.oldHeight = oldH;
     }
 
     /****/
@@ -63,8 +66,8 @@ public class StatisticsController implements StatisticsControllerInterface {
             if (difficulty != Difficulty.PERSONALIZED) {
                 final CategoryAxis yAxis = new CategoryAxis();
                 final NumberAxis xAxis = new NumberAxis();
-                final BarChart<Number, String> easyChart = new BarChart<>(xAxis, yAxis);
-                easyChart.setTitle("Classification-" + difficulty);
+                final BarChart<Number, String> chart = new BarChart<>(xAxis, yAxis);
+                chart.setTitle("Top 10-" + difficulty);
                 xAxis.setLabel("POINTS");
                 yAxis.setLabel("PLAYERS");
                 //for players (messi in ordine decr.)
@@ -78,11 +81,11 @@ public class StatisticsController implements StatisticsControllerInterface {
                     final XYChart.Series<Number,String> p = new XYChart.Series<>();
                     p.setName("Palyerrr"+ String.valueOf(i));
                     p.getData().add(new XYChart.Data<>(i*100,p.getName()));
-                    easyChart.getData().add(p);
+                    chart.getData().add(p);
                 }
-                easyChart.setLegendVisible(false);
-                easyChart.setStyle("-fx-font-size: 18");
-                this.vBox.getChildren().add(easyChart);
+                chart.setLegendVisible(false);
+                chart.setStyle("-fx-font-size: 15");
+                this.vBox.getChildren().add(chart);
             }
         }
     }
@@ -90,12 +93,12 @@ public class StatisticsController implements StatisticsControllerInterface {
     private void addGeneralChart() {
         final ObservableList<PieChart.Data> generalPieChartData =
                 FXCollections.observableArrayList(
-                new PieChart.Data("Win", 120), //...TOTAL WIN
-                new PieChart.Data("Defeat", 100)); //..TOTAL DEFEAT
+                new PieChart.Data("Wins", 120), //...TOTAL WIN
+                new PieChart.Data("Losses", 100)); //..TOTAL DEFEAT
         final PieChart generalChart = new PieChart(generalPieChartData);
-        generalChart.setData(generalPieChartData);
+        //generalChart.setData(generalPieChartData);
         generalChart.setTitle("General");
-        generalChart.setStyle("-fx-font-size: 18");
+        generalChart.setStyle("-fx-font-size: 15");
         generalChart.setLegendSide(Side.LEFT);
         this.vBox.getChildren().add(generalChart);
     }
@@ -106,7 +109,7 @@ public class StatisticsController implements StatisticsControllerInterface {
         final RWSettings rwSett = new RWSettingsImpl();
         final Parent pane = FXMLLoader.load(ClassLoader.getSystemResource("layouts/mainStatistics.fxml"));
         final Stage stage = (Stage) this.rootPane.getScene().getWindow();
-        final Scene scene = new Scene(pane, stage.getScene().getWidth(), stage.getScene().getHeight());
+        final Scene scene = new Scene(pane, this.oldWidth, this.oldHeight);
         scene.getStylesheets().add(ClassLoader.getSystemResource("css/" + rwSett.getCss()).toExternalForm());
         stage.setScene(scene);
     }
