9cb057409ea02aa7a2bdaef7569882cc3fa59a5a
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-29 19:06:14 +0200

    Created Factory to create a TimerView
    default or personalized

---
+++ src/main/java/timer/TimerViewFactory.java
@@ -0,0 +1,32 @@
+package timer;
+
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+/**
+ * A Factory to modify a timer visual representation easily.
+ */
+public interface TimerViewFactory {
+
+    /**
+     * Creates a default configuration of a Timers GUI.
+     * @param timer
+     * The Timer to visualize.
+     * @return
+     * Returns a TimerView to add to the GUI.
+     */
+    TimerView defaultTimerDisplay(Timer timer);
+
+    /**
+     * Creates a default configuration of a Timers GUI.
+     * @param timer
+     * The Timer to visualize.
+     * @param font 
+     * The Font of the numbers displayed.
+     * @param color
+     * The Color of the number displayed.
+     * @return
+     * Returns a TimerView to add to the GUI.
+     */
+    TimerView personalizedTimerDisplay(Timer timer, Font font, Color color);
+}
