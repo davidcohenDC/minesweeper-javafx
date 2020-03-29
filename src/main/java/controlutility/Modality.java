af45bf61b771923b0165ab344a429ee74e7d5318
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-29 23:52:40 +0200

    added missing javadoc for Modality and Difficulty

--- src/main/java/controlutility/Modality.java
+++ src/main/java/controlutility/Modality.java
@@ -8,14 +8,14 @@ public enum Modality {
     /**
      * Standard mode.
      */
-    STANDARD("STD"), 
+    STANDARD("STD"),
 
     /**
      * One versus one.
      * 
      * Play against another player.
      */
-    ONE_VS_ONE("OVO"), 
+    ONE_VS_ONE("OVO"),
 
     /**
      * Beat the Timer.
@@ -26,10 +26,20 @@ public enum Modality {
 
     private final String directoryName;
 
+    /**
+     * Creates a new {@link Modality}.
+     * 
+     * @param name
+     *                 The name to associate to the modality.
+     */
     Modality(final String name) {
         this.directoryName = name;
     }
 
+    /**
+     * @return Returns the directory inside <i>.minesweeper/score_files/</i> where
+     *         the files for the modality's files are stored.
+     */
     public String getDirectoryName() {
         return this.directoryName;
     }
