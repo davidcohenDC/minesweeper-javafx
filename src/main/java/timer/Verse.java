b49b33480161f8ce4c250ebfc018b2be98327add
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-30 19:27:55 +0200

    Added limit field in Verse

--- src/main/java/timer/Verse.java
+++ src/main/java/timer/Verse.java
@@ -8,29 +8,37 @@ public enum Verse {
     /**
      * Increases a value by 1 step each time.
      */
-    UP(1),
+    UP(1, 999),
 
     /**
      * Decreases a value by 1 step each time.
      */
-    DOWN(-1);
+    DOWN(-1, 0);
 
-    private final int verseIncrementValue;
-
+    private final int verseValue;
+    private final int limit;
     /**
      * Creates a new Verse.
      * 
      * @param incrementValue
      *                           the step which will increment a value
      */
-    Verse(final int incrementValue) {
-        verseIncrementValue = incrementValue;
+    Verse(final int verseValue, final int incrementLimit) {
+        this.verseValue = verseValue;
+        this.limit = incrementLimit;
     }
 
     /**
      * @return Returns the step's value.
      */
     public int getVerseIncrementValue() {
-        return verseIncrementValue;
+        return this.verseValue;
+    }
+
+    /**
+     * @return Returns the limit value.
+     */
+    public int getLimit() {
+        return this.limit;
     }
 }
