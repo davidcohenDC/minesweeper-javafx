c0fbbb8ac18c6fb505a7c2cf869d5a6ee6643a9f
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-30 14:28:24 +0200

    created class for testing timer functionalities

---
+++ src/test/java/timer/TestTimer.java
@@ -0,0 +1,23 @@
+package timer;
+
+import org.junit.jupiter.api.Test;
+/**
+ * A Class to test {@link Timer} functionalities are working.
+ */
+class TimerTest {
+
+    @Test
+    public void standardTimerTest() {
+        
+    }
+
+    @Test
+    public void beatTheTimerTest() {
+        
+    }
+
+    @Test
+    public void doubleTimerTest() {
+        
+    }
+}
