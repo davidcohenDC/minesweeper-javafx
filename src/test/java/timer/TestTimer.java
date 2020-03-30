ff2cd895b6e9ac2e4673e083815449cee98469fd
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-30 18:47:24 +0200

    START OVER ON TIMER
    Sadly the timer package was not accurate enough so a new way of handling
    the time of the game must be found
    
    New implementation will focus on System.currentTimeMillis(); and the
    system way of keeping time instead of constantly relaying on
    Thread.sleep() and increasing a value because it was highly inprecise

--- src/test/java/timer/TestTimer.java
+++ src/test/java/timer/TestTimer.java
@@ -29,130 +29,169 @@ class TestTimer {
 
     @Test
     public void standardTimerTest() {
-
+        int goodtests = 0;
+        int badtests = 0;
+        for (int i = 0; i < 10; i++) {
         final Timer t = f.createTimerForStandardMode();
-        final int time = rnd.nextInt(MAX_SLEEP_TIME);
-
-        // timer has not started so it should be on hold
-        assertTrue(t.isPaused());
-
-        // timer starts running
-        t.startTimer();
-        assertFalse(t.isPaused());
-
-        try {
-            // after a random time the timer gets paused
-            Thread.sleep(1_000 * time);
-            t.pause();
-            assertTrue(t.isPaused());
-            assertEquals(time, t.getValue());
-
-            // then it starts again
-            t.play();
-            assertFalse(t.isPaused());
-            Thread.sleep(1_000 * time);
-
-            // until it gets stopped
-            t.stopTimer();
-            assertTrue(t.isPaused());
-
-            // play should not reactivate the timer after it was stopped
-            t.play();
-            assertTrue(t.isPaused());
-            assertEquals(time * 2, t.getValue());
-
-            // the timer's value should be increased
-            assertTrue(t.getValue() > time);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            fail();
-        }
-    }
-
-    @Test
-    public void beatTheTimerTest() {
-
-        final Timer t = f.createTimerForBeatTheTimerMode(MAX_SLEEP_TIME * 2);
-        final int time = rnd.nextInt(MAX_SLEEP_TIME);
-
-        // timer has not started so it should be on hold
-        assertTrue(t.isPaused());
+        final int time = 10;
+        int c = 0;
+//        System.out.println("time = " + time);
 
         // timer starts running
-        t.startTimer();
-        assertFalse(t.isPaused());
 
-        try {
             // after a random time the timer gets paused
-            Thread.sleep(1_000 * time);
-            t.pause();
-            assertTrue(t.isPaused());
-            assertEquals(time, t.getValue());
+            t.startTimer();
+            long start = System.currentTimeMillis();
+            while (System.currentTimeMillis() - start < 11) {
+                c++;
+                try {
+                    Thread.sleep(0, 1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(c);
+            }
+            int tvalue = t.getValue();
+////            System.out.println(t.getValue());
+//            System.out.println("Test " + i + " actual " + time + " timer " + t.getValue() + " " + (time == t.getValue()));
+            System.out.println(tvalue);
+            if (c == tvalue) {
+                goodtests++;
+            } else {
+                badtests++;
+            }
+//            assertEquals(time, t.getValue());
 
-            // then it starts again
-            t.play();
-            assertFalse(t.isPaused());
-            Thread.sleep(1_000 * time);
-
-            // until it gets stopped
             t.stopTimer();
-            assertTrue(t.isPaused());
 
-            // play should not reactivate the timer after it was stopped
-            t.play();
-            assertTrue(t.isPaused());
-            assertEquals(0, t.getValue());
 
-            // the timer's value should be decreased
-            assertTrue(t.getValue() < time);
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            fail();
         }
+        System.out.println();
+        System.out.println();
+        System.out.println("good " + goodtests + " bad " + badtests);
     }
 
-    @Test
-    public void doubleTimerTest() {
-        final DoubleTimer dt = f.createTimersFor1vs1Mode();
-        final int time = rnd.nextInt(MAX_SLEEP_TIME);
-
-        // timer has not started so it should be on hold
-        assertTrue(dt.getPlayer1Timer().isPaused());
-        assertTrue(dt.getPlayer2Timer().isPaused());
-
-        // player 1 should go first while player 2 is on hold
-        dt.startTimers();
-        assertFalse(dt.getPlayer1Timer().isPaused());
-        assertTrue(dt.getPlayer2Timer().isPaused());
-
-        try {
-            // after a random time the player switch turns
-            Thread.sleep(1_000 * time);
-            dt.switchTurn();
-            assertTrue(dt.getPlayer1Timer().isPaused());
-            assertFalse(dt.getPlayer2Timer().isPaused());
-            assertEquals(0, dt.getPlayer2Timer().getValue());
-            assertEquals(time, dt.getPlayer1Timer().getValue());
-
-            // then it starts again
-            Thread.sleep(1_000 * time);
-            dt.switchTurn();
-            assertTrue(dt.getPlayer2Timer().isPaused());
-            assertFalse(dt.getPlayer1Timer().isPaused());
-            assertEquals(time, dt.getPlayer2Timer().getValue());
-            assertEquals(time, dt.getPlayer1Timer().getValue());
-
-            // both timers get stopped
-            dt.stopTimers();
-            assertTrue(dt.getPlayer1Timer().isPaused());
-            assertTrue(dt.getPlayer2Timer().isPaused());
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            fail();
-        }
-
-    }
+//    assertTrue(t.isPaused());
+//
+//    // timer starts running
+//    t.startTimer();
+////    assertFalse(t.isPaused());
+//
+//    try {
+//        // after a random time the timer gets paused
+//        Thread.sleep(1 * time);
+//        System.out.println("actual " + time + " timer = " + t.getValue() + (time == t.getValue()));
+//
+//        t.pause();
+//        assertTrue(t.isPaused());
+////        assertEquals(time, t.getValue());
+//
+//        // then it starts again
+//        t.play();
+//        assertFalse(t.isPaused());
+//        Thread.sleep(1 * time);
+//        // until it gets stopped
+//        t.stopTimer();
+//        assertTrue(t.isPaused());
+//
+//        // play should not reactivate the timer after it was stopped
+//        t.play();
+//        assertTrue(t.isPaused());
+////        assertEquals(time * 2, t.getValue());
+//
+//    } catch (InterruptedException e) {
+//        e.printStackTrace();
+//        fail();
+//    }
+//    }
+//    @Test
+//    public void beatTheTimerTest() {
+//
+//        final Timer t = f.createTimerForBeatTheTimerMode(MAX_SLEEP_TIME * 2);
+//        final int time = rnd.nextInt(MAX_SLEEP_TIME);
+//
+//        // timer has not started so it should be on hold
+//        assertTrue(t.isPaused());
+//
+//        // timer starts running
+//        t.startTimer();
+//        assertFalse(t.isPaused());
+//
+//        try {
+//            // after a random time the timer gets paused
+//            Thread.sleep(1_000 * time);
+//            t.pause();
+//            assertTrue(t.isPaused());
+//            assertEquals(time, t.getValue());
+//
+//            // then it starts again
+//            t.play();
+//            assertFalse(t.isPaused());
+//            Thread.sleep(1_000 * time);
+//
+//            // until it gets stopped
+//            t.stopTimer();
+//            assertTrue(t.isPaused());
+//
+//            // play should not reactivate the timer after it was stopped
+//            t.play();
+//            assertTrue(t.isPaused());
+//            assertEquals(0, t.getValue());
+//
+//            // the timer's value should be decreased
+//            if (time == 0) {
+//                assertEquals(time, t.getValue());
+//            } else {
+//                assertTrue(t.getValue() < time);
+//            }
+//
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//            fail();
+//        }
+//    }
+//
+//    @Test
+//    public void doubleTimerTest() {
+//        final DoubleTimer dt = f.createTimersFor1vs1Mode();
+//        final int time = rnd.nextInt(MAX_SLEEP_TIME);
+//
+//        // timer has not started so it should be on hold
+//        assertTrue(dt.getPlayer1Timer().isPaused());
+//        assertTrue(dt.getPlayer2Timer().isPaused());
+//
+//        // player 1 should go first while player 2 is on hold
+//        dt.startTimers();
+//        assertFalse(dt.getPlayer1Timer().isPaused());
+//        assertTrue(dt.getPlayer2Timer().isPaused());
+//
+//        try {
+//            // after a random time the player switch turns
+//            Thread.sleep(1_000 * time);
+//            dt.switchTurn();
+//            assertTrue(dt.getPlayer1Timer().isPaused());
+//            assertFalse(dt.getPlayer2Timer().isPaused());
+//            assertEquals(0, dt.getPlayer2Timer().getValue());
+//            assertEquals(time, dt.getPlayer1Timer().getValue());
+//
+//            // then it starts again
+//            Thread.sleep(1_000 * time);
+//            dt.switchTurn();
+//            assertTrue(dt.getPlayer2Timer().isPaused());
+//            assertFalse(dt.getPlayer1Timer().isPaused());
+//            assertEquals(time, dt.getPlayer2Timer().getValue());
+//            assertEquals(time, dt.getPlayer1Timer().getValue());
+//
+//            // both timers get stopped
+//            dt.stopTimers();
+//            assertTrue(dt.getPlayer1Timer().isPaused());
+//            assertTrue(dt.getPlayer2Timer().isPaused());
+//
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//            fail();
+//        }
+//
+//    }
 }
