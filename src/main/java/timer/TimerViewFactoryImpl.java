a192f1b9132e5efde3924e39ff6e0b8ee163dc1c
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-31 17:17:49 +0200

    Implemented TimerViewFactory

---
+++ src/main/java/timer/TimerViewFactoryImpl.java
@@ -0,0 +1,19 @@
+package timer;
+
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class TimerViewFactoryImpl implements TimerViewFactory {
+
+    @Override
+    public final TimerView createDefault(final Timer timer, final Label label) {
+        return new TimerViewImpl(timer, label, Color.BLACK, Color.LIGHTGRAY, Font.getDefault());
+    }
+
+    @Override
+    public final TimerView createPersonalized(final Timer timer, final Label label, final Color numbersColor, final Color backgroundColor, final Font font) {
+        return new TimerViewImpl(timer, label, numbersColor, backgroundColor, font);
+    }
+
+}
