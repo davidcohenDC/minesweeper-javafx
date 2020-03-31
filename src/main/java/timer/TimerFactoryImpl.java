028a5dbef716cc29facc3ad1b46d029b2efd6745
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-03-31 16:47:14 +0200

    Added TimerFactory

---
+++ src/main/java/timer/TimerFactoryImpl.java
@@ -0,0 +1,20 @@
+package timer;
+
+public class TimerFactoryImpl implements TimerFactory {
+
+    @Override
+    public final Timer createTimerForStandardMode() {
+        return new TimerImpl(0, Verse.UP);
+    }
+
+    @Override
+    public final Timer createTimerForBeatTheTimerMode(final int amountOfTime) {
+        return new TimerImpl(amountOfTime, Verse.DOWN);
+    }
+
+    @Override
+    public final MultipleTimers createTimersFor1vs1Mode() {
+        return new DoubleTimerImpl();
+    }
+
+}
