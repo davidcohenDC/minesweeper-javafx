a30526b0ed89840083af38d81eff8110f3e60152
Cohen David <david.cohen@studio.unibo.it>
2020-04-02 17:29:09 +0200

    Create graphics template

---
+++ src/main/java/GameGraphics/GraphicsController.java
@@ -0,0 +1,46 @@
+package GameGraphics;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import controlutility.AlertStyle;
+import controlutility.AlertStyleImpl;
+import controlutility.RWSettings;
+import controlutility.RWSettingsImpl;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * The Controller related to the SinglePlayer.fxml GUI.
+ */
+public final class GraphicsController implements GraphicsControllerImpl{
+
+
+    @Override
+    public void initialize() throws IOException {
+
+    }
+
+    @Override
+    public void btPlayGame() throws IOException {
+
+    }
+
+    @Override
+    public void btRestart() throws IOException {
+
+    }
+
+    @Override
+    public void exit() throws IOException {
+
+    }
+}
