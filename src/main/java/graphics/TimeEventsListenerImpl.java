758c2b676e6b1b1894e64c3ad9203e5a16a43a0b
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-04-11 20:11:33 +0200

    format file

--- src/main/java/graphics/TimeEventsListenerImpl.java
+++ src/main/java/graphics/TimeEventsListenerImpl.java
@@ -1,14 +1,16 @@
 package graphics;
 
 import timer.OutOfTimeEvent;
+
 /**
- * The implementation of {@link TimeEventsListener} to handle {@link OutOfTimeEvent}. 
+ * The implementation of {@link TimeEventsListener} to handle
+ * {@link OutOfTimeEvent}.
  */
 public class TimeEventsListenerImpl implements TimeEventsListener {
 
     @Override
     public final void timeEventOccured(final OutOfTimeEvent event) {
-        //TODO Game ending procedure.
+        // TODO Game ending procedure.
         System.out.println("EVENTO TIMER SCATENATO!!");
     }
 
