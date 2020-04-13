4111e2b1e4290d9e9f2622bf21fa840fd6da0187
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-04-13 21:37:19 +0200

    Fixed check at beginning of player.
    if adversary was empty it threw an exception.

--- src/main/java/scoresystem/PlayerImpl.java
+++ src/main/java/scoresystem/PlayerImpl.java
@@ -33,7 +33,7 @@ public class PlayerImpl implements Player {
      */
     protected PlayerImpl(final String name, final Modality gameMode, final Difficulty difficulty,
             final Optional<String> adversaryName) {
-        check(name.equals(adversaryName.get()), "Two different players can't have the same name");
+        check(adversaryName.isPresent() && name.equals(adversaryName.get()), "Two different players can't have the same name");
         this.name = name;
         this.gameMode = gameMode;
         this.difficuly = difficulty;
