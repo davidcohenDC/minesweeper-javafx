e1a3818616847c54e9fc222316593ad152dd8cb9
Luigi Olivieri <luigi.olivieri@studio.unibo.it>
2020-04-15 16:26:34 +0200

    Converter's method is now more function oriented

--- src/main/java/scoresystem/Converter.java
+++ src/main/java/scoresystem/Converter.java
@@ -3,8 +3,8 @@ package scoresystem;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * A converter to easily convert lists and files.
@@ -20,25 +20,31 @@ public final class Converter {
 
     /**
      * Converts a File in a List of its Lines.
+     * 
      * @param path
-     * The Path of the file to convert.
+     *                      The Path of the file to convert.
      * @param separator
-     * The separator to make sure no line with the wrong format is included.
-     * @return Returns a {@link List} of strings containing the each line of the file.
+     *                      The separator to make sure no line with the wrong format
+     *                      is included.
+     * @return Returns a {@link List} of strings containing the each line of the
+     *         file.<br>
+     *         If the conversion goes wrong it will return an empty
+     *         {@link List}.
      */
     public static List<String> fileToList(final Path path, final String separator) {
-        final List<String> lines = new ArrayList<>();
         try {
-            List.of(Files.lines(path).toArray()).stream()
-                                                .filter(line -> String.valueOf(line).contains(separator))
-                                                .forEach(line -> lines.add(String.valueOf(line)));
+            return List.of(Files.lines(path).toArray()).stream()
+                                                       .map(String::valueOf)
+                                                       .filter(line -> line.contains(separator))
+                                                       .collect(Collectors.toList());
         } catch (IOException e) {
             if (Files.exists(path)) {
                 System.err.println("The lines from the file were not transfered correctly.");
-                System.err.println(lines);
+            } else {
+                System.err.println("The path: " + path.toString() + " does not exist.");
             }
         }
-        return lines;
+        return List.of();
     }
 
 }
